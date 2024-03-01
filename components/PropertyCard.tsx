badb0a2c7d88c4d867762fd1802c4d3bf20d807a
Milad Joodi <winbeta.com@gmail.com>
2024-03-01 22:05:20 +0330

    cards

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "lucide-react";
+import { Bath, Bed, Link, Square } from "lucide-react";
 import { propertiesList } from "./Features";
 import Image from "next/image";
 
@@ -15,20 +15,41 @@ const PropertyCard = ({
     sqft,
 }: PropertyCardProps) => {
     return (
-            <div className="border rounded-md overflow-hidden">
-                <div className="aspect-[16/10] overflow-hidden">
-                    <Image
-                        src={image}
-                        alt={name}
-                        width={400}
-                        height={420}
-                        className="w-full transition-transform group-hover:scale-105"
-                    />
+        <div className="border rounded-md overflow-hidden">
+            <div className="aspect-[16/10] overflow-hidden">
+                <Image
+                    src={image}
+                    alt={name}
+                    width={400}
+                    height={420}
+                    className="w-full transition-transform group-hover:scale-105"
+                />
+            </div>
+            <div className="p-6 space-y-6">
+                <p className="text-lg font-semibold">{name}</p>
+                <div className="grid grid-cols-3 items-center divide-x border-y border-border/50">
+                    <p className="py-2 flex items-center gap-2 justify-center">
+                        <Square className="w-4 h-4" />
+                        {sqft} sqft
+                    </p>
+                    <p className="py-2 flex items-center gap-2 justify-center">
+                        <Bed className="w-4 h-4" />
+                        {beds} Beds
+                    </p>
+                    <p className="py-2 flex items-center gap-2 justify-center">
+                        <Bath className="w-4 h-4" />
+                        {baths} Baths
+                    </p>
                 </div>
-                <div>
-                    <p className="text-lg">{name}</p>
+                <div className="flex justify-between">
+                    <div>
+                        <p className="text-muted-foreground">Price</p>
+                        <p className="font-semibold">{price}</p>
+                    </div>
+                    <div className="text-muted-foreground">Rating</div>
                 </div>
             </div>
+        </div>
     );
 }
 
