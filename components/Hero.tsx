1ac0e3d50ab26002f207800840d63de3aa19bf78
Milad Joodi <winbeta.com@gmail.com>
2024-03-01 18:35:59 +0330

    Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <div className="min-h-screen hero">
+            Hero
+        </div>
+    );
+}
+
+export default Hero;
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const navList = [
     {
@@ -13,22 +14,27 @@ const navList = [
 
 const Navigation = () => {
     return (
-        <nav>
-            {/* LEFT */}
-            <p>Estate</p>
+        <nav className="fixed w-screen px-4 top-2 z-50">
+            <div className="py-3 container flex items-center justify-between bg-background rounded-md z-50">
+                {/* LEFT */}
+                <p>Estate</p>
 
-            {/* CENTER */}
-            <ul>
-                {navList.map(nav => (
-                    <li key={nav.title}>
-                        <Link href={nav.href}>{nav.title}</Link>
-                    </li>
-                ))}
-            </ul>
+                {/* CENTER */}
+                <ul className="flex items-center gap-4 ">
+                    {navList.map(nav => (
+                        <li key={nav.title}>
+                            <Link href={nav.href} className="text-muted-foreground hover:text-primary hover:underline">{nav.title}</Link>
+                        </li>
+                    ))}
+                </ul>
 
-            {/* RIGHT */}
-            <div>
-            
+                {/* RIGHT */}
+                <div>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>U</AvatarFallback>
+                    </Avatar>
+                </div>
             </div>
 
         </nav>
