4c1cb240aeedcabb3b7ab497cd084918cbba8587
Milad Joodi <winbeta.com@gmail.com>
2024-03-01 19:18:01 +0330

    search component

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,8 +1,23 @@
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
+
 const SearchForm = () => {
+
+    const form = useForm()
     return (
-        <div>
-            Search
-        </div>
+        <Form {...form}>
+            <FormField control={form.control} name="address" render={({ field }) => {
+                return (
+                    <FormItem>
+                        <FormLabel>Address</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Search by Address"/>
+                        </FormControl>
+                    </FormItem>
+                )
+            }} />
+        </Form>
     );
 }
 
